9efc817ea139e1eaecd7e3778616c6eb9b3f4225
Dakshata <22-783@apsdhaulakuan.onmicrosoft.com>
2025-02-25 01:07:05 +0530

    password

---
+++ practice-2/sortingfruit.js
@@ -0,0 +1,16 @@
+let fruit = ["🍎", "🍊", "🍎", "🍎", "🍊"]
+let appleShelf = document.getElementById("apple-shelf")
+let orangeShelf = document.getElementById("orange-shelf")
+
+function sortfruits(){
+    for (let i = 0; i<fruit.length; i ++){
+    if (fruit[i]===`🍎`)
+        {
+        appleShelf.textContent += `🍎`
+    }
+    else if (fruit[i]===`🍊`){
+        orangeShelf.textContent += `🍊`
+    }
+    }
+}
+sortfruits()

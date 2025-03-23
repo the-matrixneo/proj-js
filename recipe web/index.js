66a9b95f3d68f192e3d5f1b24e8c0a68cf318e54
Dakshata <22-783@apsdhaulakuan.onmicrosoft.com>
2025-03-24 00:55:31 +0530

    ..

---
+++ recipe web/index.js
@@ -0,0 +1,7 @@
+const searchbtn = document.querySelector('#search') //for id
+const submitbtn = document.querySelector('#submit')
+const recipecontainer = document.querySelector(".recipe-container")//for class
+searchbtn.addEventListener('click', function(){
+  console.log("search button clicked")
+})
+

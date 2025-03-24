cb4397d81538dcc74d88ee6da5d92714e90cabaa
Dakshata <22-783@apsdhaulakuan.onmicrosoft.com>
2025-03-24 23:46:34 +0530

    recipe

--- recipe web/index.js
+++ recipe web/index.js
@@ -1,7 +1,24 @@
-const searchbtn = document.querySelector('#search') //for id
-const submitbtn = document.querySelector('#submit')
+const searchbtn = document.querySelector('#submit') //for id
+const submitbtn = document.querySelector('#search')
 const recipecontainer = document.querySelector(".recipe-container")//for class
-searchbtn.addEventListener('click', function(){
-  console.log("search button clicked")
+// function to get recipes
+const fetchrecipes = async (recipes) => {
+  const data = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${recipes}`)
+  const response = await data.json()
+  
+  response.meals.forEach(meal => {
+    //console.log(meal)
+    const recipeDiv = document.createElement('div') //create a div in js
+    recipeDiv.classList.add('recipe') // class in js
+    recipeDiv.innerHTML  = `
+    <img src ="${meal.strMealThumb}" alt =  `
+
+  })
+ } 
+submitbtn.addEventListener('click', function(a){
+  a.preventDefault()  //to prevent the default action of the form
+  const searchinput = searchbtn.value.trim()
+  fetchrecipes(searchinput)
+  //console.log("search button clicked")
 })
 
